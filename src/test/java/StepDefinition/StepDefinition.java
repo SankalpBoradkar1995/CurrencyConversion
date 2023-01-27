683cb5997bae0d1dd50c3fd4910a65f299ead535
Sankalp <boradkarsankalp6@gmail.com.com>
2023-01-27 15:07:12 +0530

    Updated variable references in StepDefinition file

--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -18,51 +18,51 @@ import io.restassured.RestAssured;
 @RunWith(Cucumber.class)
 public class StepDefinition {
 	Function<String, BigDecimal> refToFunction;
-	ParameterPojoClass obj1;
-	String response;
+	ParameterPojoClass refToPojoClass;
+	String responseOne;
 	public static BigDecimal currencyGBP;
 	BigDecimal currencyDogeCoin;
-	String response2;
+	String responseTwo;
 
 	@Given("^All query parameters assigned to parameterPojoClass$")
 	public void SetQueryParams() {
 		RestAssured.baseURI = "https://pro-api.coinmarketcap.com";
 
-		obj1 = new ParameterPojoClass();
-		obj1.setAmount(10000000);
-		obj1.setIdOne("3541");
-		obj1.setConvertOne("GBP");
-		obj1.setSourceURI("/v2/tools/price-conversion");
-		obj1.setIdTwo("2791");
-		obj1.setConvertTwo("DOGE");
+		refToPojoClass = new ParameterPojoClass();
+		refToPojoClass.setAmount(10000000);
+		refToPojoClass.setIdOne("3541");
+		refToPojoClass.setConvertOne("GBP");
+		refToPojoClass.setSourceURI("/v2/tools/price-conversion");
+		refToPojoClass.setIdTwo("2791");
+		refToPojoClass.setConvertTwo("DOGE");
 	}
 
 	@When("^User makes call to price-conversion api$")
 	public void MakeApiCallGBP() {
-		response = given().queryParam("amount", obj1.getAmount()).queryParam("id", obj1.getIdOne())
-				.queryParam("convert", obj1.getConvertOne()).header("X-CMC_PRO_API_KEY", obj1.getApiKey())
-				.header("Content-Type", "application/json").when().get(obj1.getSourceURI()).then().extract().response().asString();
+		responseOne = given().queryParam("amount", refToPojoClass.getAmount()).queryParam("id", refToPojoClass.getIdOne())
+				.queryParam("convert", refToPojoClass.getConvertOne()).header("X-CMC_PRO_API_KEY", refToPojoClass.getApiKey())
+				.header("Content-Type", "application/json").when().get(refToPojoClass.getSourceURI()).then().extract().response().asString();
 	}
 
 	@Then("^Input currency is changed to GBP currency$")
 	public void GetConvertedCurrencyGBP() {
 		refToFunction = Utilities::JsonParserGBP;
-		currencyGBP = refToFunction.apply(response);
-		System.out.println("Base currency was: Guatemalan Quetzal with amount: " + obj1.getAmount());
+		currencyGBP = refToFunction.apply(responseOne);
+		System.out.println("Base currency was: Guatemalan Quetzal with amount: " + refToPojoClass.getAmount());
 		System.out.println("Converted GBP are: " + currencyGBP);
 	}
 
 	@When("^User pass converted GBP to API$")
 	public void MakeApiCallD() {
-		response2 = given().queryParam("amount", currencyGBP).queryParam("id", obj1.getIdTwo())
-				.queryParam("convert", obj1.getConvertTwo()).header("X-CMC_PRO_API_KEY", obj1.getApiKey())
-				.header("Content-Type", "application/json").when().get(obj1.getSourceURI()).then().extract().response().asString();
+		responseTwo = given().queryParam("amount", currencyGBP).queryParam("id", refToPojoClass.getIdTwo())
+				.queryParam("convert", refToPojoClass.getConvertTwo()).header("X-CMC_PRO_API_KEY", refToPojoClass.getApiKey())
+				.header("Content-Type", "application/json").when().get(refToPojoClass.getSourceURI()).then().extract().response().asString();
 	}
 
 	@Then("GBP currency is changed to Dogecoin")
 	public void GetConvertedCurrencyD() {
 		refToFunction = Utilities::JsonParserD;
-		currencyDogeCoin = refToFunction.apply(response2);
+		currencyDogeCoin = refToFunction.apply(responseTwo);
 		System.out.println("Base currency was: GGBP with amount: " + currencyGBP);
 		System.out.println("Converted Dogecoin are: " + currencyDogeCoin);
 	}
